5caebefda2223d9c8121a4a6bb616905b9d4abc0
Piyush Kumar <piyush.kumar@hmcts.net>
2019-11-05 16:08:58 +0000

    add implicit dependency

--- peerings.tf
+++ peerings.tf
@@ -12,8 +12,8 @@ data "azurerm_virtual_network" "hub-vnet" {
 resource "azurerm_virtual_network_peering" "hub_to_aks" {
   count                        = length(var.aks_config)
   name                         = "hub_to_${lookup(var.common_tags, "activityName")}"
-  resource_group_name          = var.rg_name
-  virtual_network_name         = var.vnet_name
+  resource_group_name          = data.azurerm_virtual_network.hub-vnet.resource_group_name
+  virtual_network_name         = data.azurerm_virtual_network.hub-vnet.name
   remote_virtual_network_id    = data.azurerm_virtual_network.aks[count.index].id
   allow_virtual_network_access = true
   depends_on                        = ["data.azurerm_virtual_network.hub-vnet"]
@@ -26,5 +26,4 @@ resource "azurerm_virtual_network_peering" "aks_to_hub" {
   virtual_network_name         = data.azurerm_virtual_network.aks[count.index].name
   remote_virtual_network_id    = data.azurerm_virtual_network.hub-vnet.id
   allow_virtual_network_access = true
-  depends_on                        = ["data.azurerm_virtual_network.hub-vnet"]
 }
