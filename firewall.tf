57b1763a74a50714d519334ec70b10aa4c2442b5
Piyush Kumar <piyush.kumar@hmcts.net>
2019-11-06 14:08:54 +0000

    remove the local exec

--- firewall.tf
+++ firewall.tf
@@ -12,13 +12,6 @@ resource "azurerm_firewall" "main" {
   tags = var.common_tags
 }
 
-
-resource "null_resource" "azcli" {
-  provisioner "local-exec" {
-    command = "pip install --pre azure-cli --extra-index-url https://azurecliprod.blob.core.windows.net/edge"
-  }
-}
-
 resource "null_resource" "ip_config" {
   count = length(var.aks_config) > 1 ? length(var.aks_config) : 0
 
@@ -26,5 +19,5 @@ resource "null_resource" "ip_config" {
     command = "az network firewall ip-config create --firewall-name ${azurerm_firewall.main.name} --name ${var.aks_config[1]} --public-ip-address ${azurerm_public_ip.main[1].id} --resource-group ${var.rg_name} --vnet-name ${var.vnet_name}"
   }
 
-  depends_on = [azurerm_firewall.main, null_resource.azcli]
+  depends_on = [azurerm_firewall.main]
 }
