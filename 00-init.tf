63ff0c4d35d29007b4695ce2f32cd86195439d4a
Tim Jacomb <tim.jacomb@hmcts.net>
2019-11-08 12:47:14 +0000

    Disable peerings

--- 00-init.tf
+++ 00-init.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 0.11.0"
 }
 
-provider "azurerm" {
-  alias           = "aks"
-  subscription_id = lookup(var.peering_setup, "subscription")
-}
+//provider "azurerm" {
+//  alias           = "aks"
+//  subscription_id = lookup(var.peering_setup, "subscription")
+//}
--- 11-peerings.tf
+++ 11-peerings.tf
@@ -1,18 +1,18 @@
-resource "azurerm_virtual_network_peering" "hub_to_aks" {
-  count                        = lookup(var.peering_setup, "subscription", "") == "" ? 0 : 1
-  name                         = "hub_to_aks"
-  resource_group_name          = data.azurerm_virtual_network.hub_vnet.name
-  virtual_network_name         = data.azurerm_virtual_network.hub_vnet.name
-  remote_virtual_network_id    = data.azurerm_virtual_network.aks[0].id
-  allow_virtual_network_access = true
-}
-
-resource "azurerm_virtual_network_peering" "aks_to_hub" {
-  count                        = lookup(var.peering_setup, "subscription", "") == "" ? 0 : 1
-  provider                     = azurerm.aks
-  name                         = "aks_to_hub"
-  resource_group_name          = data.azurerm_virtual_network.aks[0].resource_group_name
-  virtual_network_name         = data.azurerm_virtual_network.aks[0].name
-  remote_virtual_network_id    = data.azurerm_virtual_network.hub_vnet.id
-  allow_virtual_network_access = true
-}
+//resource "azurerm_virtual_network_peering" "hub_to_aks" {
+//  count                        = lookup(var.peering_setup, "subscription", "") == "" ? 0 : 1
+//  name                         = "hub_to_aks"
+//  resource_group_name          = data.azurerm_virtual_network.hub_vnet.name
+//  virtual_network_name         = data.azurerm_virtual_network.hub_vnet.name
+//  remote_virtual_network_id    = data.azurerm_virtual_network.aks[0].id
+//  allow_virtual_network_access = true
+//}
+//
+//resource "azurerm_virtual_network_peering" "aks_to_hub" {
+//  count                        = lookup(var.peering_setup, "subscription", "") == "" ? 0 : 1
+//  provider                     = azurerm.aks
+//  name                         = "aks_to_hub"
+//  resource_group_name          = data.azurerm_virtual_network.aks[0].resource_group_name
+//  virtual_network_name         = data.azurerm_virtual_network.aks[0].name
+//  remote_virtual_network_id    = data.azurerm_virtual_network.hub_vnet.id
+//  allow_virtual_network_access = true
+//}
