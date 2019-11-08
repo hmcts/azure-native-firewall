d17d57ca19fedfa72005a2ba22e00fc8a0425129
Tim Jacomb <tim.jacomb@hmcts.net>
2019-11-08 12:59:25 +0000

    Hack

--- 00-init.tf
+++ 00-init.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 0.11.0"
 }
 
-//provider "azurerm" {
-//  alias           = "aks"
-//  subscription_id = lookup(var.peering_setup, "subscription")
-//}
+provider "azurerm" {
+  alias           = "aks"
+  subscription_id = "b72ab7b7-723f-4b18-b6f6-03b0f2c6a1bb"
+}
